3a728869b6d1393b4262f96a80f4d332792fef45
sergij14 <sergi.jajanidze@gmail.com>
2021-01-18 01:09:14 +0400

    edited js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-"use strict";
+import "core-js/stable";
+
+("use strict");
 
 // Selecting
 const btnModal = document.querySelectorAll(".btn-modal");
@@ -7,11 +9,26 @@ const overlay = document.querySelector(".overlay");
 /////////////////////////////////////
 // Modal Window
 
+const disableScrolling = function () {
+  const x = window.scrollX;
+  const y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
+};
+
+const enableScrolling = function () {
+  window.onscroll = function () {};
+};
+
 const modalWindow = function (id) {
   const modal = document.getElementById(id);
   modal.classList.toggle("modal--show");
   overlay.classList.toggle("overlay--show");
   overlay.addEventListener("click", () => modalWindow(id));
+  modal.classList.contains("modal--show")
+    ? disableScrolling()
+    : enableScrolling();
 };
 
 btnModal.forEach((btn) => {
